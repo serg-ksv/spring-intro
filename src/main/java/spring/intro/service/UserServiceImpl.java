08340a58757ec345df3639aa125d33685fbdf186
serg-ksv <serg.ksv1@gmail.com>
2020-06-03 10:58:09 +0300

    Removed setter and annotation Qualifier

--- src/main/java/spring/intro/service/UserServiceImpl.java
+++ src/main/java/spring/intro/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package spring.intro.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import spring.intro.dao.UserDao;
 import spring.intro.model.User;
@@ -10,6 +9,11 @@ import spring.intro.model.User;
 @Service
 public class UserServiceImpl implements UserService {
     private UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void add(User user) {
@@ -19,11 +23,5 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> listUsers() {
         return userDao.listUsers();
-    }
-
-    @Autowired
-    @Qualifier(value = "userDaoImpl")
-    public void setUserDao(UserDao userDao) {
-        this.userDao = userDao;
     }
 }
